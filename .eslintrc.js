911603a81ec1637e0bfe8c26f5a007aaf50c674c
Jordan Harband <ljharb@gmail.com>
2020-05-18 14:29:37 -0700

    update all the things

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,21 @@
 module.exports = {
 	env: {
 		node: true,
-		es6: true
+		es6: true,
 	},
-	extends: '@ljharb',
+	'extends': '@ljharb',
 	parserOptions: {
 		ecmaVersion: 2018,
 	},
 	rules: {
-		camelcase: ['error', {
-			properties: 'never',
-			ignoreDestructuring: false,
-			ignoreImports: false,
-			allow: []
-		}],
+		camelcase: [
+			'error', {
+				properties: 'never',
+				ignoreDestructuring: false,
+				ignoreImports: false,
+				allow: [],
+			},
+		],
 		'comma-dangle': ['error', 'always-multiline'],
 		'dot-notation': [2, { allowKeywords: true }],
 		'func-style': 'warn',
@@ -24,7 +26,11 @@ module.exports = {
 		'max-statements-per-line': ['error', { max: 2 }],
 		'no-cond-assign': ['error', 'always'],
 		'no-console': 'off',
-		'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+		'no-unused-vars': [
+			'error', {
+				vars: 'all', args: 'after-used', ignoreRestSiblings: true,
+			},
+		],
 		'object-shorthand': ['error', 'always'],
 		'prefer-arrow-callback': 'error',
 		'prefer-const': 'error',
