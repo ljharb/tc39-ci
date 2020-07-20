b8243890434b250e71c983f86041a8a109ce2e84
Ryan Block <rblock@gmail.com>
2020-07-20 11:24:08 -0700

    Should fix pagination issue for branches with >30 commits

--- src/http/post-preview-000u-000r/_verify.js
+++ src/http/post-preview-000u-000r/_verify.js
@@ -3,6 +3,28 @@
 const data = require('@begin/data');
 const { get } = require('tiny-json-http');
 
+const getCommits = async (url) => {
+	let page = 1;
+	const commits = [];
+	const urlPage = () => `${url}&page=${page}&per_page=100`;
+
+	const getter = async (getting) => {
+		const result = await get({ url: getting });
+		const { body, headers } = result;
+		if (body.length) { commits.push(...body); }
+		if (headers.link || headers.Link) {
+			const link = headers.link || headers.Link;
+			const hasNextLink = link && link.includes('rel="next"');
+			if (hasNextLink) {
+				page += 1;
+				await getter(urlPage());
+			}
+		}
+	};
+	await getter(urlPage());
+	return commits;
+};
+
 /**
  * Verify that the received payload is indeed a real PR + SHA
  * If so, pass it along
@@ -15,10 +37,10 @@ module.exports = async function verify(req) {
 		const base = 'https://api.github.com';
 		const url = `${base}/repos/${user}/${repo}/pulls/${pr}/commits`;
 
-		const commits = await get({ url });
+		const commits = await getCommits(url);
 
 		// Is this an actual SHA on the repo?
-		const shaIsReal = commits.body.some((c) => c.sha === sha);
+		const shaIsReal = commits.some((c) => c.sha === sha);
 
 		// Have we seen this SHA before and published its preview?
 		const shaHasBeenSeen = await data.get({
@@ -33,6 +55,7 @@ module.exports = async function verify(req) {
 		}
 		return void undefined;
 	} catch (err) {
+		console.error(err);
 		return {
 			statusCode: 403,
 		};
